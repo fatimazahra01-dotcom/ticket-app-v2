7943b47df5d3c64b272a90acc8d5147fbc16479a
fatimazahra01-dotcom <fatimazahraajmilou@gmail.com>
2025-10-05 01:38:20 +0100

    Update App.js

--- frontend/projet/src/App.js
+++ frontend/projet/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link,Navigate } from "react-router-dom";
-import LoginPage from "./Component/LoginPage"; // ta page de login
+import LoginPage from "./Component/LoginPage"; 
 import "./App.css";
 import AddTicket from "./Component/AddTicket";
 import TicketTable from "./Component/TicketTable";
@@ -12,7 +12,6 @@ export default function App() {
   return (
     <Router>
       <div>
-        {/* Logo + Titre */}
         <img
           src="vivo image.jpg"
           alt="logo"
@@ -21,22 +20,20 @@ export default function App() {
         />
       
 
-        {/* Barre de navigation */}
         <nav className="navbar" style={{ display: "flex", justifyContent: "center", gap: "20px", marginBottom: "20px" }}>
-          <Link to="/add" className="nav-link">➕ Ajouter un ticket</Link>
-          <Link to="/dashboard" className="nav-link">📋 Voir les tickets</Link>
-          <Link to="/login" className="nav-link">🔑 Connexion</Link>
+          <Link to="/add" className="nav-link"> Ajouter un ticket</Link>
+          <Link to="/dashboard" className="nav-link"> Voir les tickets</Link>
+          <Link to="/login" className="nav-link"> Connexion</Link>
         </nav>
 
-        {/* Routes */}
+     
  <Routes>
   <Route path="/" element={<Navigate to="/add" />} />
   <Route path="/add" element={<AddTicket />} />
   <Route path="/dashboard" element={<TicketTable />} />
   <Route path="/login" element={<LoginPage />} />
 </Routes>
-
-<Footer /> {/* ✅ Footer affiché sur toutes les pages */}
+<Footer /> 
 
 
       </div>
